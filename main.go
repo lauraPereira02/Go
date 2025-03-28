9095748d148ef356edbc73cbc6401069b9789264
laura.p.santos8@aluno.senai.br <laura.p.santos8@aluno.senai.br>
2025-03-28 09:06:25 -0300

    operadores logicos senha

--- main.go
+++ main.go
@@ -3,18 +3,18 @@ package main
 import "fmt"
 
 func main (){
-	var numero1 int
-	var numero2 int
+	var usuario string
+	var senha string
 
-	fmt.Print("digite um numero")
- 	fmt.Scan(&numero1)
-	 fmt.Print("digite outro numero")
- 	fmt.Scan(&numero2)
- 	
-	fmt.Println("a soma é", numero1 + numero2)
-	fmt.Println("a subtraçao é:", numero1 - numero2)
-	fmt.Println("a divisao é ", numero1 / numero2)
-	fmt.Println("a multiplicaçao é:", numero1 * numero2)
-	fmt.Println("o resto da divisao:", numero1 % numero2)
+	fmt.Print("digite o usuario")
+ 	fmt.Scan(&usuario)
+	 fmt.Print("digite a senha")
+ 	fmt.Scan(&senha)
 	
-}
+	if senha == "1234" && usuario == "admin" {
+        fmt.Println("acesso permitido")
+		} else {
+			fmt.Println("acesso negado")
+		}
+	}
+
