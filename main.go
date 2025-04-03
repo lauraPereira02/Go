0bfb541554d2f2027934a662aed80c19ec505653
laura.p.santos8@aluno.senai.br <laura.p.santos8@aluno.senai.br>
2025-04-03 09:25:03 -0300

    slice

--- main.go
+++ main.go
@@ -9,5 +9,20 @@ import "fmt"
  fmt.Println(nomes) 
  nomes[3] = "clara"
  fmt.Println(nomes)
+// Slice
+var score = []int{100,200,300,400}
+fmt.Println(score)
+score[1] = 2
+fmt.Println(score, len(score), cap(score))
+rengeOne := score [1:3]
+fmt.Println(rengeOne)
+rangetwo := score[2:]
+fmt.Println(rangetwo)
+rangethree := score [:3]
+fmt.Println(rangethree)
 
- }
+var superherois = []string{"homem aranha","homem de ferro","thor"}
+fmt.Println(superherois)
+superherois = append(superherois, "bem dez","huck")
+fmt.Println(superherois, len(superherois), cap(superherois))
+}
