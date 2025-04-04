042c573fc22a8d538b4fbdbd5297df1d7cfee229
laura.p.santos8@aluno.senai.br <laura.p.santos8@aluno.senai.br>
2025-04-04 10:03:22 -0300

    for

--- main.go
+++ main.go
@@ -23,4 +23,23 @@ import (
  sort.Strings(names)
  fmt.Println(names)
  fmt.Println(sort.SearchStrings(names,"Caroline"))
+ x:=0
+
+for x <= 5 {fmt.Println(x) 
+	x++}
+
+for i:=0; i <5; i++{
+	fmt.Println("for 2:", i)}
+
+for i :=0; i <len(names); i++{
+	fmt.Println(names[i])
+	}
+
+for index, value := range names{
+	fmt.Println("O indice é", index, "o valor é ",value)
+	}
+
+for index, value := range ages{	
+	fmt.Println("O indice é", index, "o valor é ",value)
 }
+  }
