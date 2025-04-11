27bcd41bea09bf33487be1ce4cff0364623d35a2
laura.p.santos8@aluno.senai.br <laura.p.santos8@aluno.senai.br>
2025-04-11 13:33:12 -0300

    greeting

--- main.go
+++ main.go
@@ -1,32 +1,18 @@
 package main
 
-import "fmt"
+import ("fmt"
+)
+
+func sayGreeting(nome string){
+   fmt.Println("Olá!", nome) 
+}
+
+func addNumber (numero1 int, numero2 int) int {
+   return numero1 + numero2
+}
 
 func main() {
-	age := 45
-	fmt.Println(age<= 50)
-   fmt.Println(age >= 50)
-   fmt.Println(age == 50)
-   fmt.Println(age!= 50)
-
-   if age < 50 {
-      fmt.Println("menor que 30anos")
-   } else if age < 40 {
-      fmt.Println("menor que 40anos")
-   } else {
-      fmt.Println("não é menor que 40anos")
+   sayGreeting("sara")
+   resultado := addNumber(10, 20)
+   fmt.Println(resultado)
    }
-
-   names := []string{"Isa", "madu", "maju", "leo", "lala", "lili" }
-
-   for index, value := range names {
-      if index == 1 {
-         fmt.Println("continue apos a posiçao", index, "e valor", value)
-         continue
-      }
-      if index > 2 {
-         fmt.Println("sair apos" ,index)
-      }
-      fmt.Println("valor: ", value)
-   }
-}
