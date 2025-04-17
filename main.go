49379f9dba0256887488996dce8f54cc2e5c3487
laura.p.santos8@aluno.senai.br <laura.p.santos8@aluno.senai.br>
2025-04-17 08:36:02 -0300

    operacaoBasica

--- main.go
+++ main.go
@@ -1,18 +1,31 @@
 package main
 
-import ("fmt"
+import (
+   "fmt"
 )
 
-func sayGreeting(nome string){
-   fmt.Println("Olá!", nome) 
+func dividir (dividendo int, divisor int)(int, string){
+   if divisor == 0 {
+   return 0,"erro na divisao por zero"
 }
-
-func addNumber (numero1 int, numero2 int) int {
-   return numero1 + numero2
+   return dividendo / divisor, "Sem erro"
+} 
+func operacaoBasica(a int, b int,)(int, int, int){
+   soma := a + b
+   multiplicacao := a * b
+   subtracao := a - b
+   return soma, multiplicacao, subtracao
 }
+func main(){
+   resultado, erro := dividir(10,2)
 
-func main() {
-   sayGreeting("sara")
-   resultado := addNumber(10, 20)
-   fmt.Println(resultado)
+   if erro != "Sem erro"{
+fmt.Printf(erro)
+   } else{
+      fmt.Println("o resultado da divisão é:",resultado, erro)
    }
+   soma, mult, sub := operacaoBasica(10,2)
+   fmt.Println(soma)
+   fmt.Println(mult)
+   fmt.Println(sub)  
+}
